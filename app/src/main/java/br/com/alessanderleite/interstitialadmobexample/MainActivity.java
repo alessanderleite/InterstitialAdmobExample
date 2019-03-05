0323c6e8fa97f1449824044254232ddcd0649768
alessanderleite <alessander.logistica@gmail.com>
2019-03-05 09:52:38 -0300

    initialize the InterstitialAd inside onCreate method

--- app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        // Create the InterstitialAd and set the adunitId.
+        interstitialAd = new InterstitialAd(this);
+        // Defined in res/values/strings.xml
+        interstitialAd.setAdUnitId(getString(R.string.ad_unit_id));
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                startGame();
+            }
+        });
+        
     }
 }
