6890ce15ffb1a25c866b8862b306759653f06a9b
alessanderleite <alessander.logistica@gmail.com>
2019-03-05 09:45:37 -0300

    declare the objects and variables inside MainActivity class

--- app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
@@ -1,9 +1,21 @@
 package br.com.alessanderleite.interstitialadmobexample;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long GAME_LENGTH_MILLISECONDS = 3000;
+
+    private InterstitialAd interstitialAd;
+    private CountDownTimer countDownTimer;
+    private Button retryButton;
+    private boolean gameIsInprogress;
+    private long timerMilliseconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
