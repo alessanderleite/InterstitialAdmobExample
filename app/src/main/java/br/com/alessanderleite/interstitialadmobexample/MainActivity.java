a0cfbc5e739b8f3b564d7c33b05abd20757c5d1b
alessanderleite <alessander.logistica@gmail.com>
2019-03-05 16:11:42 -0300

    create the method named createTimer()

--- app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/interstitialadmobexample/MainActivity.java
@@ -3,7 +3,9 @@ package br.com.alessanderleite.interstitialadmobexample;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.InterstitialAd;
@@ -39,5 +41,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Create the "retry" button, which tries to show an interstitial between game plays.
+        retryButton = findViewById(R.id.retry_button);
+        retryButton.setVisibility(View.INVISIBLE);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInterstitial();
+            }
+        });
+
+        startGame();
     }
+
+    private void createTimer(final long milliseconds) {
+        // Create the game timer, which counts down to the end of the level
+        // and shows the "retry" button.
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+
+        final TextView textView = findViewById(R.id.timer);
+
+        countDownTimer = new CountDownTimer(milliseconds, 50) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerMilliseconds = millisUntilFinished;
+                textView.setText("seconds remainig: " + ((millisUntilFinished / 1000) + 1));
+            }
+
+            @Override
+            public void onFinish() {
+                gameIsInprogress = false;
+                textView.setText("done!");
+                retryButton.setVisibility(View.VISIBLE);
+            }
+        };
+    }
+    
 }
